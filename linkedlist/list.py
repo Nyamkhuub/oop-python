c0a8ec936bb7d31c432335aa3c817936efebc32e
Nyamkhuub <15b1seas1613@stud.num.edu.mn>
2019-09-11 08:49:22 +0300

    linked list part 2

--- linkedlist/list.py
+++ linkedlist/list.py
@@ -4,8 +4,10 @@ class Node:
         self.next = None
 
 class List:
-    def __init__(self):
+    def __init__(self, *elements):
         self.header = None
+        for element in elements:
+            self.add(element)
     def add(self, value):
         if self.header == None:
             self.header = Node(value)
@@ -19,11 +21,30 @@ class List:
         while(curr != None):
             print(curr.value)
             curr = curr.next
+    def remove(self, value):
+        curr = self.header
+        prev = self.header
+        hasElement = False
+        while curr != None:
+            if curr.value == value:
+                if curr == self.header:
+                    self.header = self.header.next
+                    hasElement = True
+                    break
+                prev.next = curr.next
+                hasElement = True
+                break
+            prev = curr
+            curr = curr.next
+        if hasElement:
+            print('Amjilttai ustgalaa!')
+        else:
+            print('tuhain haij baigaa utga listend baihgui bna')
 
-test = List()
-test.add(1)
-test.add(2)
-test.add(3)
-test.add('ene bol text')
-test.add('hamgiin engiin baidlaar listiig heregjuullee!')
+test = List(1, 2, 3, 4, 'ene bol test')
+test.see()
+test.add('testing add function')
+print('-----------')
+test.remove(1)
+print('-----------')
 test.see()
