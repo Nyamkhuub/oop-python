d8095fc1af340753ad61c148a82901e1bd70b93a
Nyamkhuub <15b1seas1613@stud.num.edu.mn>
2019-09-29 10:44:10 +0300

    web framework ashiglaj uzev

---
+++ flask/test/test.py
@@ -0,0 +1,12 @@
+def main():
+    print('Test python ooroo ajillaj bna')
+
+def im():
+    print('Test python import hiigdej ajillaj bna')
+
+if __name__ == '__main__':
+    main()
+else:
+    im()
+
+
---
+++ flask/test/test1.py
@@ -0,0 +1,3 @@
+import test
+
+print('2 dahi file')
