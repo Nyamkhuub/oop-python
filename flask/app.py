ac791332d6e997042a9ba3a6dede0446431348ae
Nyamkhuub <15b1seas1613@stud.num.edu.mn>
2019-10-08 15:41:00 +0300

    flask deer template ashiglasan

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello world!'
+@app.route('/<name>')
+def index(name='Nyamkhuu'):
+    return render_template('main.html', name=name)
 
 @app.route('/about')
 def about():
